36c3032f989e82f728bac5be688d1db5b083a322
Ram Bhandari <ramjdbhandari51@gmail.com>
2019-07-25 07:02:34 +0545

    Updates

---
+++ precache-manifest.498e4d0ea6b384e056688be8ff96ebed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23aeda806c98af20b78e7d3210f80935",
+    "url": "/hackernews-ui/index.html"
+  },
+  {
+    "revision": "0c3fbd6d6cf57bc549b8",
+    "url": "/hackernews-ui/static/css/main.419d2f26.chunk.css"
+  },
+  {
+    "revision": "45a1e11c2619aab378c6",
+    "url": "/hackernews-ui/static/js/2.6416fb34.chunk.js"
+  },
+  {
+    "revision": "0c3fbd6d6cf57bc549b8",
+    "url": "/hackernews-ui/static/js/main.45f0f07f.chunk.js"
+  },
+  {
+    "revision": "6502b751e42927b4e245",
+    "url": "/hackernews-ui/static/js/runtime~main.9a3bd466.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hackernews-ui/precache-manifest.df171a087f0a16cc774a72d0ca2edeb3.js"
+  "/hackernews-ui/precache-manifest.498e4d0ea6b384e056688be8ff96ebed.js"
 );
 
 self.addEventListener('message', (event) => {
